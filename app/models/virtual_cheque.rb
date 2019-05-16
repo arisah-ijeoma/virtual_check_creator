6d158928e4b1eda1fcc5fd15bd7bf8ce9f1dc814
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-05-16 11:20:26 +0100

    make spec pass

--- app/models/virtual_cheque.rb
+++ app/models/virtual_cheque.rb
@@ -3,11 +3,19 @@
 require 'humanize'
 
 class VirtualCheque < ApplicationRecord
+  before_save :capitalize_names
   after_save :add_amount_in_words
 
   validates :recipient_name, :amount, :date, presence: true
 
   private
+
+  def capitalize_names
+    # 'and' has an exception for the titleize method in #add_amount_in_words
+    # we'll be using capitalize instead in case we have a recipient with 'And'
+    # as a name
+    self.recipient_name = recipient_name.split.map(&:capitalize).join(' ')
+  end
 
   def add_amount_in_words
     amounts = amount.to_s.split('.').map do |d|
