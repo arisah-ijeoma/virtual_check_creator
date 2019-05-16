aea5493d520c08c0dd882c2e14a6ac1987272f5e
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-05-16 11:31:41 +0100

    refactor code

--- app/models/virtual_cheque.rb
+++ app/models/virtual_cheque.rb
@@ -19,7 +19,6 @@ class VirtualCheque < ApplicationRecord
       d.sub!(/^0*/, '').to_i.humanize.delete(',').titleize.gsub('And', 'and')
     end
 
-    amount_in_words = "#{amounts[0]} dollars and #{amounts[1]} cents"
-    self.amount_in_words = amount_in_words
+    self.amount_in_words = "#{amounts[0]} dollars and #{amounts[1]} cents"
   end
 end
