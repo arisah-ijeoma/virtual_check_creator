8f720293584b52428a46af4912ea7e9184dcfd09
Ijeoma Arisah <ijeomarisah@gmail.com>
2019-05-17 12:21:42 +0100

    make spec pass

--- app/models/virtual_cheque.rb
+++ app/models/virtual_cheque.rb
@@ -4,7 +4,7 @@ require 'humanize'
 
 class VirtualCheque < ApplicationRecord
   before_save :capitalize_names
-  after_create :add_amount_in_words
+  after_create :update_amount_in_words
 
   validates :recipient_name, :amount, :date, presence: true
 
@@ -16,12 +16,19 @@ class VirtualCheque < ApplicationRecord
     self.recipient_name = recipient_name.titleize
   end
 
-  def add_amount_in_words
-    amounts = amount.to_s.split('.').map do |d|
-      d.sub!(/^0*/, '').to_i.humanize.delete(',').titleize.gsub('And', 'and')
-    end
-
-    self.amount_in_words = "#{amounts[0]} dollars and #{amounts[1]} cents"
+  def update_amount_in_words
+    self.amount_in_words = translate_amount_to_words
     save
+  end
+
+  def translate_amount_to_words
+    amn = amount.to_s.split('.')
+    amn.map.with_index do |n, i|
+      dollar_words = amn[1].to_i > 0 ? ' dollars and': ' dollars'
+      # checks for unnecessary zeros and removes them
+      # capitalizes all words apart from 'and'
+      n.sub!(/^0*/, '').to_i > 0 && (n.to_i.humanize.delete(',').titleize.gsub('And', 'and') +
+        (i == 0 ? dollar_words : ' cents')) || ''
+    end.join('')
   end
 end
